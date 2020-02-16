a3dae67f96d380289a2bb8253ec6c85a611b07d4
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-16 16:02:58 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,35 +1,33 @@
-
+require 'pry'
 
 class Author
+  attr_accessor :name
 
-  attr_accessor :name, :posts
-
-  @@post_count = 0
+  @@all = []
 
   def initialize(name)
     @name = name
-    @posts = []
+    @@all << self
   end
 
-  def posts
-    @posts
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(name)
-    post = Post.new(name)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
+  def self.all
     @@all
   end
 
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
 end
